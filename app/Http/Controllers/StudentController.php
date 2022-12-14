b2a499d48a86bb7e9e3a67216cf8d4ad0a9238a1
NabilaAgustina <agustinacahyani5@gmail.com>
2022-12-14 18:23:21 +0700

    Perbandingan Query Builder & Eloquent

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,14 +4,43 @@ namespace App\Http\Controllers;
 
 use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class StudentController extends Controller
 {
     public function index(){
-        $student = Student::all();
+        // $student = Student::all();
+        // // dd($student);
+        // return view('student', [
+        //     'studentList' => $student
+        // ]);
+
+        // Quoery builder
+        // $student = DB::table('students')->get();
         // dd($student);
-        return view('student', [
-            'studentList' => $student
-        ]);
+        // DB::table('students')->insert([
+        //     'name' => 'Tes Quoery builder',
+        //     'gender' => 'P',
+        //     'nis' => '100003',
+        //     'class_id' => 3
+        // ]);
+        // DB::table('students')->where('id', 27)->update(['name' => 'update Quoery builder']);
+        // DB::table('students')->where('id', 27)->delete();
+
+        
+        // Eloquent
+        // $student = Student::all();
+        // Student::create([
+        //     'name' => 'Tes Eloquent',
+        //     'gender' => 'P',
+        //     'nis' => '100003',
+        //     'class_id' => 3
+        // ]);
+        // dd($student);
+        // Student::findOrfail(29)->update([
+        //     'name' => 'Update Eloquent',
+        //     'class_id' => 4
+        // ]);
+        // Student::findOrfail(29)->delete();
     }
 }
